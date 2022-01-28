6ec83a3c819cfdcf8999e60116fc1a1076c91b73
DESKTOP-F8NT5Q9\Admin-pc <sowmya181992@gmail.com>
2022-01-28 21:34:36 +0530

    HomeScreen

--- samaan/src/App.js
+++ samaan/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React,{ Component } from 'react';
 import './App.css';
+import Home from './Components/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World !....
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+ render(){
+   return(
+     <div className = "App">
+       <Home/>
+     </div>
+   );
+ }
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Hello World !....
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Hello World
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ samaan/src/Components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useNavigate} from 'react-router-dom'
+
+const Home = () => {
+ return(
+<div class = "centered" >
+    <h1>Welcome to Samaan....</h1>
+   
+    <button class = "button">Login</button> 
+    <button class = "button" >SignUp</button><br/>
+     
+</div>
+
+
+
+)}
+// }
+export default Home;
