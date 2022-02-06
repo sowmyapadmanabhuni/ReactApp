cb081cd5377aa3b88e03415ce580fde140d13399
DESKTOP-F8NT5Q9\Admin-pc <sowmya181992@gmail.com>
2022-02-06 12:52:24 +0530

    dashboard initial commit

--- samaan/src/Components/Dashboard.js
+++ samaan/src/Components/Dashboard.js
@@ -7,10 +7,8 @@ const Dashboard =()=>{
    
    <header>
 		<h1>Welcome To Dashboard</h1>
-		{/* <div class="top">
-			<a href="#" class="menu_icon"><i class="material-icons">Groceries Dashboard</i></a>
-		</div> */}
 	</header>
+  
 	<nav class="menu">
 		<a href="#" class="item_menu">home</a>
 		<a href="#" class="item_menu">about</a>
@@ -18,11 +16,28 @@ const Dashboard =()=>{
 		<a href="#" class="item_menu">services</a>
 		<a href="#" class="item_menu">contact</a>
 	</nav>
-   <p style={{marginLeft:"350px",marginTop:"20px"}}>	Hi, Welcome back!<br/>
-      Your Grocery monitoring dashboard template.</p>
+   <p style={{marginLeft:"350px",marginTop:"20px"}}>	Hi, Welcome back!<br/><br/>
+      Your Grocery monitoring dashboard...</p>
 	<main>
-	
-      <div className='dashboardMenu'>
+	<div class="main-overview">
+  <div class="overviewcard">
+    <div class="overviewcard__icon">Total Products</div>
+    <div class="overviewcard__info">0</div>
+  </div>
+  <div class="overviewcard">
+    <div class="overviewcard__icon">Total Categories</div>
+    <div class="overviewcard__info">0</div>
+  </div>
+  <div class="overviewcard">
+    <div class="overviewcard__icon">Total Orders</div>
+    <div class="overviewcard__info">0</div>
+  </div>
+  <div class="overviewcard">
+    <div class="overviewcard__icon">Total Users</div>
+    <div class="overviewcard__info">0</div>
+  </div>
+</div>
+      {/* <div className='dashboardMenu'>
          <div className='overviewcard'>
          <p>Total Products<br/>0</p></div>
       </div>
@@ -37,9 +52,11 @@ const Dashboard =()=>{
       <div className='dashboardMenu'>
       <div className='overviewcard'>
          <p>Total Users<br/>0</p></div>
-      </div>
+      </div> */}
 	</main>
-   
+ 
+
+  
     </div>
    )
 }
